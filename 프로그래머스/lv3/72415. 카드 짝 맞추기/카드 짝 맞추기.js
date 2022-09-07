e4451d74c302529623d9914b2a5e336366fbd031
Hoseok Na <hsna7024@gmail.com>
2022-09-07 23:48:51 +0900

    [level 3] Title: 카드 짝 맞추기, Time: 5.95 ms, Memory: 37.8 MB -BaekjoonHub

--- 프로그래머스/lv3/72415. 카드 짝 맞추기/카드 짝 맞추기.js
+++ 프로그래머스/lv3/72415. 카드 짝 맞추기/카드 짝 맞추기.js
@@ -1,173 +1,179 @@
+let min = Infinity;
+
+function Position(y, x) {
+  this.y = y;
+  this.x = x;
+}
+
+const getPermutaions = (array, number) => {
+  const result = [];
+
+  if (number === 1) {
+    return array.map(v => [v]);
+  }
+
+  array.forEach((fixed, index) => {
+    const rest = [...array.slice(0, index), ...array.slice(index + 1)];
+    const restPermutaions = getPermutaions(rest, number - 1);
+    const combined = restPermutaions.map(v => [fixed, ...v]);
+
+    result.push(...combined);
+  });
+
+  return result;
+};
+
+const isPossibleMove = (y, x, length) =>
+  y >= 0 && y < length && x >= 0 && x < length;
+
+const ctrlMove = (y, x, dy, dx, board) => {
+  let currentY = y;
+  let currentX = x;
+  let nextY = y + dy;
+  let nextX = x + dx;
+
+  while (true) {
+    if (!isPossibleMove(nextY, nextX, board.length)) {
+      return { nextY: currentY, nextX: currentX };
+    }
+
+    if (board[nextY][nextX] !== 0) {
+      return { nextY, nextX };
+    }
+
+    currentY = nextY;
+    currentX = nextX;
+    nextY += dy;
+    nextX += dx;
+  }
+};
+
+const bfs = (start, target, board, count) => {
+  const visited = Array.from(Array(board.length), () =>
+    Array(board.length).fill(false),
+  );
+
+  const q = [{ from: start, count }];
+
+  while (q.length > 0) {
+    const {
+      from: { y, x },
+      count,
+    } = q.shift();
+
+    if (y === target.y && x === target.x) {
+      board[y][x] = 0;
+
+      return { position: target, count: count + 1 };
+    }
+
+    const dy = [1, 0, -1, 0];
+    const dx = [0, 1, 0, -1];
+
+    for (let i = 0; i < dy.length; i++) {
+      const nextY = y + dy[i];
+      const nextX = x + dx[i];
+
+      if (!isPossibleMove(nextY, nextX, board.length)) {
+        continue;
+      }
+
+      if (visited[nextY][nextX]) {
+        continue;
+      }
+
+      visited[nextY][nextX] = true;
+      q.push({ from: new Position(nextY, nextX), count: count + 1 });
+    }
+
+    for (let i = 0; i < dy.length; i++) {
+      const { nextY, nextX } = ctrlMove(y, x, dy[i], dx[i], board);
+
+      if (!visited[nextY][nextX]) {
+        visited[nextY][nextX] = true;
+        q.push({ from: new Position(nextY, nextX), count: count + 1 });
+      }
+    }
+  }
+
+  return { position: start, count: Infinity };
+};
+
+const copyBoard = board => {
+  const nextBoard = [];
+
+  board.forEach(row => nextBoard.push([...row]));
+
+  return nextBoard;
+};
+
+const dfs = (start, cardMap, cardNumbers, depth, count, board) => {
+  if (depth === cardNumbers.length) {
+    if (count < min) {
+      min = count;
+    }
+
+    return;
+  }
+
+  const firstCard = cardMap.get(cardNumbers[depth])[0];
+  const secondCard = cardMap.get(cardNumbers[depth])[1];
+
+  const normalBoard = copyBoard(board);
+  let normal = bfs(start, firstCard, normalBoard, count);
+
+  normal = bfs(normal.position, secondCard, normalBoard, normal.count);
+  dfs(
+    normal.position,
+    cardMap,
+    cardNumbers,
+    depth + 1,
+    normal.count,
+    copyBoard(normalBoard),
+  );
+
+  const reverseBoard = copyBoard(board);
+  let reverse = bfs(start, secondCard, reverseBoard, count);
+
+  reverse = bfs(reverse.position, firstCard, reverseBoard, reverse.count);
+  dfs(
+    reverse.position,
+    cardMap,
+    cardNumbers,
+    depth + 1,
+    reverse.count,
+    copyBoard(reverseBoard),
+  );
+};
+
 function solution(board, r, c) {
-  const SIZE = 4
-  const dy = [1, -1, 0, 0]
-  const dx = [0, 0, -1, 1]
-  const cardMap = new Map()
-  let min = Infinity
+  const answer = 0;
+  const cardMap = new Map();
+  const LENGTH = 4;
 
-  const getPermutaions = (array, number) => {
-    const result = []
-
-    if (number === 1) {
-      return array.map((v) => [v])
-    }
-
-    array.forEach((fixed, index) => {
-      const rest = [...array.slice(0, index), ...array.slice(index + 1)]
-      const restPermutaions = getPermutaions(rest, number - 1)
-      const combined = restPermutaions.map((v) => [fixed, ...v])
-
-      result.push(...combined)
-    })
-
-    return result
-  }
-
-  const copyBoard = (board) => board.map((columns) => [...columns])
-
-  const isPossiblePoint = (y, x) => y >= 0 && y < SIZE && x >= 0 && x < SIZE
-
-  const ctrlMove = ({ y, x, direct, board }) => {
-    let nextY = y + dy[direct]
-    let nextX = x + dx[direct]
-
-    while (isPossiblePoint(nextY, nextX)) {
-      if (board[nextY][nextX] !== 0) {
-        return { y: nextY, x: nextX }
-      }
-
-      nextY += dy[direct]
-      nextX += dx[direct]
-    }
-
-    return { y: nextY - dy[direct], x: nextX - dx[direct] }
-  }
-
-  const bfs = ({ start, target, board, count }) => {
-    const visited = Array.from(Array(SIZE), () => Array(SIZE).fill(false))
-    const q = [{ from: start, count }]
-
-    while (q.length > 0) {
-      const {
-        from: { y, x },
-        count,
-      } = q.shift()
-
-      if (y === target.y && x === target.x) {
-        board[y][x] = 0
-
-        return { position: target, count: count + 1 }
-      }
-
-      for (let i = 0; i < dy.length; i++) {
-        const nextY = y + dy[i]
-        const nextX = x + dx[i]
-
-        if (!isPossiblePoint(nextY, nextX)) {
-          continue
-        }
-
-        if (visited[nextY][nextX]) {
-          continue
-        }
-
-        visited[nextY][nextX] = true
-        q.push({ from: { y: nextY, x: nextX }, count: count + 1 })
-      }
-
-      for (let i = 0; i < dy.length; i++) {
-        const { y: nextY, x: nextX } = ctrlMove({ y, x, direct: i, board })
-
-        if (visited[nextY][nextX]) {
-          continue
-        }
-
-        visited[nextY][nextX] = true
-        q.push({ from: { y: nextY, x: nextX }, count: count + 1 })
-      }
-    }
-
-    return { position: start, count: Infinity }
-  }
-
-  const dfs = ({ start, cardMap, cardNumbers, depth, count, board }) => {
-    if (depth === cardNumbers.length) {
-      if (count < min) {
-        min = count
-      }
-
-      return
-    }
-
-    const [firstCard, secondCard] = cardMap.get(cardNumbers[depth])
-    const normalBoard = copyBoard(board)
-    let normal = bfs({ start, target: firstCard, board: normalBoard, count })
-
-    normal = bfs({
-      start: normal.position,
-      target: secondCard,
-      board: normalBoard,
-      count: normal.count,
-    })
-
-    dfs({
-      start: normal.position,
-      cardMap,
-      cardNumbers,
-      depth: depth + 1,
-      count: normal.count,
-      board: copyBoard(normalBoard),
-    })
-
-    const reverseBoard = copyBoard(board)
-    let reverse = bfs({ start, target: secondCard, board: reverseBoard, count })
-
-    reverse = bfs({
-      start: reverse.position,
-      target: firstCard,
-      board: reverseBoard,
-      count: reverse.count,
-    })
-
-    dfs({
-      start: reverse.position,
-      cardMap,
-      cardNumbers,
-      depth: depth + 1,
-      count: reverse.count,
-      board: copyBoard(reverseBoard),
-    })
-  }
-
-  for (let y = 0; y < SIZE; y++) {
-    for (let x = 0; x < SIZE; x++) {
-      const cardNumber = board[y][x]
+  for (let y = 0; y < LENGTH; y++) {
+    for (let x = 0; x < LENGTH; x++) {
+      const cardNumber = board[y][x];
 
       if (cardNumber === 0) {
-        continue
+        continue;
       }
 
-      const position = { y, x }
-
-      !cardMap.has(cardNumber)
-        ? cardMap.set(cardNumber, [position])
-        : cardMap.get(cardNumber).push(position)
+      if (!cardMap.has(cardNumber)) {
+        cardMap.set(cardNumber, [new Position(y, x)]);
+      } else {
+        cardMap.get(cardNumber).push(new Position(y, x));
+      }
     }
   }
 
-  const cardNumbers = [...cardMap].map(([cardNumber, _]) => cardNumber)
-  const combinations = getPermutaions(cardNumbers, cardNumbers.length)
+  const cardNumbers = [...cardMap].map(([cardNumber, _]) => cardNumber);
+  const combinations = getPermutaions(cardNumbers, cardNumbers.length);
 
-  combinations.forEach((currentCardNumbers) => {
-    dfs({
-      start: { y: r, x: c },
-      cardMap,
-      cardNumbers: currentCardNumbers,
-      depth: 0,
-      count: 0,
-      board,
-    })
-  })
+  const start = new Position(r, c);
+  combinations.forEach(currentCardNumbers => {
+    dfs(start, cardMap, currentCardNumbers, 0, 0, board);
+  });
 
-  return min
+  return min;
 }
